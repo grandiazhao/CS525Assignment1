ea82d14f5b97c787a58817ecb07232700f261bff
zhuli-julie <lzhu34@hawk.iit.edu>
2016-09-22 23:18:22 -0500

    comment
    搜 peipei   替换下

--- storage_mgr.c
+++ storage_mgr.c
@@ -135,7 +135,7 @@ RC createPageFile(char* fileName)
         printf("Can not create the file %s!!",fileName);
         return RC_FILE_OPEN_FAILED;
     }
-    
+
     //record the file header, and write it into the beginning of the file
 	initDataBaseHeader();
     p_dataBaseHeader->maxPageCount=1;
@@ -158,10 +158,10 @@ RC createPageFile(char* fileName)
 
 /*********************************************************************************
  * Function:        openPageFile
- * Description:     open an exist page file
+ * Description:     open an exist page file, read the file header to get the information 
+ *                  and save these into handle
  * Input:           char* fileName: file name
-                    SM_FileHandle *fHandle: file handler
- * Output:          None
+ * Output:          SM_FileHandle *fHandle: file handle
  * Return:          RC: return code
  **********************************************************************************/
 RC openPageFile(char *fileName, SM_FileHandle *fHandle)
@@ -186,196 +186,393 @@ RC openPageFile(char *fileName, SM_FileHandle *fHandle)
         return RC_FILE_OPEN_FAILED;
     }
 
-    //create a new dataBaseHeader,record the fileName and pointer
+    //create a new dataBaseHeader,record the pointer
     initDataBaseHeader();
 	p_dataBaseHeader->filePointer = fp;
+
+    //read file header from file, and save the information in file handle
     readDataBaseHeader(fp);
     fHandle->mgmtInfo=p_dataBaseHeader;
     fHandle->fileName=fileName;
     fHandle->curPagePos=0;
     fHandle->totalNumPages=p_dataBaseHeader->maxPageCount;
+
     return RC_OK;
 }
 
+/*********************************************************************************
+ * Function:        closePageFile
+ * Description:     close a file
+ * Input:           SM_FileHandle *fHandle: file handle
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC closePageFile(SM_FileHandle *fHandle)
 {
+    // check if the handle is valid
     if(fHandle==0||fHandle->mgmtInfo==0)
     {
         printf("The fileHandle is Empty!!!");
         return RC_FILE_HANDLE_NOT_INIT;
     }
 
+    // update the information in header
     p_dataBaseHeader=fHandle->mgmtInfo;
+
+    // close file
     fclose(p_dataBaseHeader->filePointer);
+
+    // write header to the file
+    peipei1
+
     //we should delete the dataBaseHeader stored in mgmtInfo and then delete the fHandle
     free(p_dataBaseHeader->additionalInfo);
     free(p_dataBaseHeader);
     p_dataBaseHeader=0;
 	fHandle->mgmtInfo = 0;
+
     return RC_OK;
 }
 
+/*********************************************************************************
+ * Function:        destroyPageFile
+ * Description:     destroy a file
+ * Input:           char *fileName: file name
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC destroyPageFile(char *fileName)
 {
-    //check whether the file is exsit or not
+    //check whether the file is exsit. 
     int ret=_access(fileName,0);
+
+    // Return error if it doesn't exit.
     if(ret!=0)
     {
         printf("The file %s does not exsit!",fileName);
         return RC_FILE_NOT_FOUND;
     }
+
+    // remove file and check if it is successfully.
     ret=remove(fileName);
     if(ret!=0)
     {
         printf("Error in remove the file! Please check the permission!");
         return RC_FILE_REMOVE_FAILED;
     }
+
     return RC_OK;
 }
 
-RC check_readBlock_commonError(SM_FileHandle* fHandle, SM_PageHandle memPage)
+/*********************************************************************************
+ * Function:        check_readBlock_commonError
+ * Description:     check if handle is valid
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
+RC check_readBlock_commonError(SM_FileHandle* fHandle)
 {
+    // check if handle is valid
 	if (fHandle == 0 || fHandle->mgmtInfo == 0)
 	{
 		printf("The fileHandle is Empty!!!");
 		return RC_FILE_HANDLE_NOT_INIT;
 	}
+
 	return RC_OK;
 }
 
-
+/*********************************************************************************
+ * Function:        readBlock
+ * Description:     read the pageNumth block from a file into memPage. 
+ * Called By:       readCurrentBlock
+                    readFirstBlock
+                    readLastBlock
+                    readNextBlock
+                    readPreviousBlock
+ * Input:           int pageNum: the sequence number of page that need to be read
+                    SM_FileHandle* fHandle: file handle
+ * Output:          SM_PageHandle memPage: the page handle that will be written
+ * Return:          RC: return code
+ **********************************************************************************/
 RC readBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    //get information from handle
 	p_dataBaseHeader = fHandle->mgmtInfo;
+
+    // check if pageNumber is valid.
 	if (pageNum >= p_dataBaseHeader->maxPageCount)
 	{
 		printf("PAGENUM exceed MAXPAGECOUNT");
 		return RC_READ_NON_EXISTING_PAGE;
 	}
+
 	fHandle->curPagePos = pageNum;
+
+    // reset the file pointer to the beginning
     rewind(p_dataBaseHeader->filePointer);
+
+    // get the position of pageNumth block
     fseek(p_dataBaseHeader->filePointer,PAGE_SIZE*pageNum + p_dataBaseHeader->sizeofHeader,SEEK_SET);
+
+    //read the pageNumth block
     int readsize=fread(memPage,1,PAGE_SIZE,p_dataBaseHeader->filePointer);
 	if (readsize != PAGE_SIZE)
 	{
 		return RC_ERROR;
 	}
+
     return RC_OK;
 }
+/*********************************************************************************
+ * Function:        getBlockPos
+ * Description:     get the block position in a file. 
+ * Input:           SM_FileHandle *fHandle: file handle
+ * Output:          None
+ * Return:          int: the block position
+ **********************************************************************************/
+peipei3
 
+/*********************************************************************************
+ * Function:        readCurrentBlock
+ * Description:     read the current block from a file into memPage. 
+ * Calls:           readBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          SM_PageHandle memPage: the page handle that will be written
+ * Return:          RC: return code
+ **********************************************************************************/
 RC readCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
 
+    // check if current page position is valid.
 	if (fHandle->curPagePos >= fHandle->totalNumPages)
 	{
 		printf("CurrentPagePos not in TotalPageNum");
 		return RC_READ_NON_EXISTING_PAGE;
 	}
 
+    //read current block
     return readBlock(fHandle->curPagePos,fHandle,memPage);
 }
 
+
+/*********************************************************************************
+ * Function:        readFirstBlock
+ * Description:     read the first block from a file into memPage. 
+ * Calls:           readBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          SM_PageHandle memPage: the page handle that will be written
+ * Return:          RC: return code
+ **********************************************************************************/
 RC readFirstBlock(SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    //read the first block
     return readBlock(0,fHandle,memPage);
 }
 
+/*********************************************************************************
+ * Function:        readLastBlock
+ * Description:     read the last block from a file into memPage. 
+ * Calls:           readBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          SM_PageHandle memPage: the page handle that will be written
+ * Return:          RC: return code
+ **********************************************************************************/
 RC readLastBlock(SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    // read last block
     return readBlock(fHandle->totalNumPages-1,fHandle,memPage);
 }
 
+/*********************************************************************************
+ * Function:        readNextBlock
+ * Description:     read the next block from a file into memPage. 
+ * Calls:           readBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          SM_PageHandle memPage: the page handle that will be written
+ * Return:          RC: return code
+ **********************************************************************************/
 RC readNextBlock(SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    // check if current page is the last one. Return error if it is.
     if(fHandle->curPagePos>=(fHandle->totalNumPages-1))
     {
 		printf("Current Page is the last Block!");
         return RC_READ_NON_EXISTING_PAGE;
     }
+
+    // read the next block
     return readBlock(fHandle->curPagePos+1,fHandle,memPage);
 }
 
+/*********************************************************************************
+ * Function:        readPreviousBlock
+ * Description:     read the previous block from a file into memPage. 
+ * Calls:           readBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          SM_PageHandle memPage: the page handle that will be written
+ * Return:          RC: return code
+ **********************************************************************************/
 RC readPreviousBlock(SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    // check if current page is the first one or invalid. Return error if it is.
 	if (fHandle->curPagePos > fHandle->totalNumPages || fHandle->curPagePos==0)
 	{
 		printf("Current Page is the first Block or current Page exceed last Block!");
 		return RC_READ_NON_EXISTING_PAGE;
 	}
+
+    // read the previous block
     return readBlock(fHandle->curPagePos-1,fHandle,memPage);
 }
 
+/*********************************************************************************
+ * Function:        writeBlock
+ * Description:     write the pageNumth block from a memPage into file. 
+ * Called By:       writeCurrentBlock
+ * Input:           int pageNum: the sequence number of page that need to be written
+                    SM_FileHandle* fHandle: file handle
+                    SM_PageHandle memPage: the page handle that will be written
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC writeBlock(int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    //get information from handle
 	p_dataBaseHeader = fHandle->mgmtInfo;
+
+    // check if pageNum is valid
     if(pageNum>=p_dataBaseHeader->maxPageCount)
     {
 		printf("The PageNum Exceed the MaxPageCount, Can not Write to Invalid Page!");
         return RC_WRITE_NON_EXISTING_PAGE;
     }
+
+    //reset the file pointer to the beginning
     rewind(p_dataBaseHeader->filePointer);
+
+    //get to the pageNumth position
     fseek(p_dataBaseHeader->filePointer,PAGE_SIZE*pageNum+p_dataBaseHeader->sizeofHeader,SEEK_SET);
+
+    // write data from memPage to file
     fwrite(memPage,1,PAGE_SIZE,p_dataBaseHeader->filePointer);
 	fflush(p_dataBaseHeader->filePointer);
+
     return RC_OK;
 }
 
+/*********************************************************************************
+ * Function:        writeCurrentBlock
+ * Description:     write the current block from a memPage into file. 
+ * Calls:           writeBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+                    SM_PageHandle memPage: the page handle that write from
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC writeCurrentBlock(SM_FileHandle *fHandle, SM_PageHandle memPage)
 {
-	RC check = check_readBlock_commonError(fHandle, memPage);
+    //check if handle given is valid
+	RC check = check_readBlock_commonError(fHandle);
 	if (check != RC_OK) return check;
+
+    //chekc if current page position is valid
 	if (fHandle->curPagePos >= fHandle->totalNumPages||fHandle->curPagePos<0)
 	{
 		printf("the CurrentPage is not Exist! Have you modified the FileHandle outside the Program???");
 		return RC_WRITE_NON_EXISTING_PAGE;
 	}
+
+    //write block
     return writeBlock(fHandle->curPagePos,fHandle,memPage);
 }
 
+/*********************************************************************************
+ * Function:        appendEmptyBlock
+ * Description:     append an new empty block filled with zero bytes at the end of the file.
+ * Called By:       ensureCapacity
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC appendEmptyBlock(SM_FileHandle *fHandle)
 {
+    //check if handle given is valid
     if(fHandle==0)
     {
         printf("The fileHandle is NULL!!!, Function will exit.");
         return RC_FILE_HANDLE_NOT_INIT;
     }
+
+    //get information from handle
     p_dataBaseHeader=(DataBaseHeader*)fHandle->mgmtInfo;
     FILE* fp=p_dataBaseHeader->filePointer;
+
+    // get the last position of file
     rewind(fp);
     fseek(fp,0,SEEK_END);
 
+    // increase the number of pages in file
     int num=p_dataBaseHeader->maxPageCount+1;
+
+    // write a page of zero bytes at the end of the file
 	char* data = (char*)calloc(PAGE_SIZE, 1);
     fwrite(data,1,PAGE_SIZE,fp);
 
+    //update the handle information
     fHandle->totalNumPages=num;
     p_dataBaseHeader->maxPageCount=num;
 	fHandle->curPagePos = num - 1;
 	p_dataBaseHeader->currentPage = num - 1;
+
     return RC_OK;
 }
 
+/*********************************************************************************
+ * Function:        ensureCapacity
+ * Description:     increase the number of pages to numberOfPages if it is less than that.
+ * Calls:           appendEmptyBlock
+ * Input:           SM_FileHandle* fHandle: file handle
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC ensureCapacity(int numberOfPages, SM_FileHandle *fHandle)
 {
+    // use a loop to check and increase the number of pages
     while(fHandle->totalNumPages<numberOfPages)
     {
         RC ret=appendEmptyBlock(fHandle);
 		if (ret != RC_OK) return ret;
     }
+    
 	return RC_OK;
 }
 
