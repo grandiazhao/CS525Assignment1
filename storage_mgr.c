61dcdf3c9e260971b6e91bf771f3d685849de35f
zhuli-julie <lzhu34@hawk.iit.edu>
2016-09-22 21:05:10 -0500

    test
    test

--- storage_mgr.c
+++ storage_mgr.c
@@ -33,6 +33,8 @@ void writePageHeader(FILE* fp)
 }*/
 
 
+/*  this file header contains basic file information, 
+ *   and stored in the beginning of file     */
 typedef struct DataBaseHeader{
 	FILE* filePointer;
 	int currentPage;
@@ -41,9 +43,16 @@ typedef struct DataBaseHeader{
 	int sizeofHeader;
 }DataBaseHeader;
 
-//this is a databaseheader used in program to help read a single database
+//this is a databaseheader used in program to help read a page file
 DataBaseHeader* p_dataBaseHeader = 0;
 
+/*********************************************************************************
+  *Function:        initDataBaseHeader
+  *Description:     intial a file header 
+  *Input:           None
+  *Output:          None
+  *Return:          None
+**********************************************************************************/
 void initDataBaseHeader()
 {
     p_dataBaseHeader=(DataBaseHeader*)malloc(sizeof(DataBaseHeader));
@@ -54,16 +63,31 @@ void initDataBaseHeader()
     p_dataBaseHeader->sizeofHeader=128;
 }
 
+/*********************************************************************************
+ * Function:        writeDataBaseHeader
+ * Description:     Write file header into the beginning of a file
+ * Input:           FILE* fp: file pointer
+ * Output:          None
+ * Return:          None
+ **********************************************************************************/
 void writeDataBaseHeader(FILE* fp)
 {
     fwrite(&p_dataBaseHeader->currentPage,sizeof(int),1,fp);
     fwrite(&p_dataBaseHeader->maxPageCount,sizeof(int),1,fp);
+    // allocate memory for other info that may be used except currentPage and maxPageCount
     char* data=(char*)calloc(p_dataBaseHeader->sizeofHeader-sizeof(int)*2,1);
     fwrite(data,1,p_dataBaseHeader->sizeofHeader-sizeof(int)*2,fp);
 	fflush(fp);
 	free(data);
 }
 
+/*********************************************************************************
+ * Function:        readDataBaseHeader
+ * Description:     Read file header from the beginning of a file
+ * Input:           FILE* fp: file pointer
+ * Output:          None
+ * Return:          None
+ **********************************************************************************/
 void readDataBaseHeader(FILE* fp)
 {
     fread(&p_dataBaseHeader->currentPage,sizeof(int),1,fp);
@@ -73,60 +97,95 @@ void readDataBaseHeader(FILE* fp)
     fread(p_dataBaseHeader->additionalInfo,1,length,fp);
 }
 
-
+/*********************************************************************************
+ * Function:        initStorageManager
+ * Description:     initial storageManager
+ * Input:           None
+ * Output:          None
+ * Return:          None
+ **********************************************************************************/
 void initStorageManager()
 {
     p_dataBaseHeader=0;
 }
 
+/*********************************************************************************
+ * Function:        createPageFile
+ * Description:     create a new page file with one page filled with '\0'
+ * Input:           char* fileName: file name
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC createPageFile(char* fileName)
 {
     //check whether the file exsit, since we want to create, the file shouldn't exsit
 	int ret = _access(fileName,0);
     
+    //if the file already exit, do not create and return error.
     if(ret==0)
     {
         printf("The file %s already exsit! Do you mean to open the file?",fileName);
         return RC_FILE_ALREADY_EXIST;
     }
-    //create the file
+
+    //create the file with mode "wb", and check if it has been successfully created
     FILE* fp=fopen(fileName,"wb");
     if(fp==0)
     {
         printf("Can not create the file %s!!",fileName);
         return RC_FILE_OPEN_FAILED;
     }
-    //record the fileName and pointer
+    
+    //record the file header, and write it into the beginning of the file
 	initDataBaseHeader();
     p_dataBaseHeader->maxPageCount=1;
     p_dataBaseHeader->currentPage=0;
     p_dataBaseHeader->filePointer=fp;
     writeDataBaseHeader(fp);
+
+    // write the page with '\0' into the file
 	char* data=(char*)calloc(PAGE_SIZE,1);
 	fwrite(data, 1, PAGE_SIZE, fp);
-	fflush(fp);
-	fclose(fp);
+    fflush(fp);
+
+    fclose(fp);
+
+    // free data
+    free(data);
+    
     return RC_OK;
 }
 
+/*********************************************************************************
+ * Function:        openPageFile
+ * Description:     open an exist page file
+ * Input:           char* fileName: file name
+                    SM_FileHandle *fHandle: file handler
+ * Output:          None
+ * Return:          RC: return code
+ **********************************************************************************/
 RC openPageFile(char *fileName, SM_FileHandle *fHandle)
 {
     //This function is almost the same as createPageFile
 
-    //check whether the file exsit
+    //check whether the file exsit.
     int ret=_access(fileName,0|2);
+
+    // Return error if the file doesn't exit.
     if(ret!=0)
     {
         printf("The file %s does not exsit or have no permit to write!",fileName);
         return RC_FILE_NOT_FOUND;
     }
-    //open the file
+
+    //open the file with mode 'rb', and check if it has been successfully created
     FILE* fp=fopen(fileName,"rb+");
     if(fp==0)
     {
         printf("Can not open the file %s!!",fileName);
         return RC_FILE_OPEN_FAILED;
     }
+
     //create a new dataBaseHeader,record the fileName and pointer
     initDataBaseHeader();
 	p_dataBaseHeader->filePointer = fp;
